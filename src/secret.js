76ca2ac2f9db2b4aa729360f4f98bc50607c1771
Egill Smári Snorrasons <47045338+egillsmari@users.noreply.github.com>
2021-03-06 12:42:56 +0000

    Update secret.js

--- src/secret.js
+++ src/secret.js
@@ -9,13 +9,13 @@
   // Pokemoney Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "AIzaSyBzYn-Z0Etq2TlFwILi2TH13qHT68B16kM",
-    authDomain: "third-summer-293619.firebaseapp.com",
-    projectId: "third-summer-293619",
-    storageBucket: "third-summer-293619.appspot.com",
-    messagingSenderId: "672506894926",
-    appId: "1:672506894926:web:e033c4d663a7381003411f",
-    measurementId: "G-T4LVH6Q8QN"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
