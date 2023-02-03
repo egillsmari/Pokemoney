28984aa162855b5fedd820eb6c4f6cc82681499f
Egill Smári Snorrasons <47045338+egillsmari@users.noreply.github.com>
2023-02-03 16:02:16 +0000

    Update enviroment.js

--- config/enviroment.js
+++ config/enviroment.js
@@ -1,12 +1,6 @@
 const environments = {
 	staging: {
-		FIREBASE_API_KEY: "AIzaSyBzYn-Z0Etq2TlFwILi2TH13qHT68B16kM",
-		FIREBASE_AUTH_DOMAIN: "third-summer-293619.firebaseapp.com",
-		FIREBASE_DATABASE_URL: 'XXXX',
-		FIREBASE_PROJECT_ID: "third-summer-293619",
-		FIREBASE_STORAGE_BUCKET: "third-summer-293619.appspot.com",
-		FIREBASE_MESSAGING_SENDER_ID: '672506894926',
-		GOOGLE_CLOUD_VISION_API_KEY: 'AIzaSyBzYn-Z0Etq2TlFwILi2TH13qHT68B16kM'
+
 	},
 	production: {
 	}
